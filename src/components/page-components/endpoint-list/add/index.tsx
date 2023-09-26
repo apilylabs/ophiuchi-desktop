edcde64eae3daad5da49ea7a7936a20de2de17b0
cheeselemon <cheeselemon.yarn@gmail.com>
2023-09-26 11:28:04 +0900

    fix: add button

--- src/components/page-components/endpoint-list/add/index.tsx
+++ src/components/page-components/endpoint-list/add/index.tsx
@@ -133,13 +133,14 @@ export default function EndpointAddSideComponent({
                             name="port"
                             min={1}
                             max={65535}
+                            required={true}
                             className="p-2 bg-transparent border-b border-b-gray-600 caret-gray-600"
                             placeholder="3000"
                           />
                         </div>
                         <div className="">
                           <button
-                            type="button"
+                            type="submit"
                             className="block rounded-md bg-indigo-500 px-3 py-2 text-center text-sm font-semibold text-white hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-500"
                           >
                             Add
