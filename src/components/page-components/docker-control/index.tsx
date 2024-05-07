a1b0615c957d97ed931e7b7d20505a57bb46af48
cheeselemon <cheeselemon.yarn@gmail.com>
2024-05-07 18:05:29 +0900

    feat: refactor docker module

---
+++ src/components/page-components/docker-control/index.tsx
@@ -0,0 +1,121 @@
+import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
+import { BaseDirectory, readTextFile, writeTextFile } from "@tauri-apps/api/fs";
+import { appDataDir, resolveResource } from "@tauri-apps/api/path";
+import { Command } from "@tauri-apps/api/shell";
+import { useCallback, useState } from "react";
+import { EndpointData } from "../proxy-list/add-new";
+import DockerLogModal from "../proxy-list/docker-log";
+
+export default function DockerControl({
+  endpointList,
+}: {
+  endpointList: EndpointData[];
+}) {
+  const [dockerProcessStream, setDockerProcessStream] = useState<any>("");
+  const [dockerModalOpen, setDockerModalOpen] = useState(false);
+  const [dockerNeedsRestart, setDockerNeedsRestart] = useState(false);
+
+  const appendDockerProcessStream = useCallback((line: any) => {
+    if (typeof line === "string") {
+      setDockerProcessStream((prev: any) => prev + `\n${line}`);
+    } else {
+      setDockerProcessStream((prev: any) => prev + line);
+    }
+  }, []);
+
+  const stopDocker = async () => {
+    const appDataDirPath = await appDataDir();
+    const command = new Command("stop-docker-compose", [
+      "compose",
+      "-f",
+      `${appDataDirPath}/docker-compose.yml`,
+      "down",
+    ]);
+    command.on("close", (data) => {
+      if (data.code == 0) {
+        appendDockerProcessStream(`✅ Stopping Docker successfully finished.`);
+      } else {
+        appendDockerProcessStream(
+          `🚨 Stopping Docker failed with code ${data.code} and signal ${data.signal}`
+        );
+      }
+      appendDockerProcessStream("💤 Waiting for docker to settle...");
+    });
+    command.on("error", (error) => console.error(`command error: "${error}"`));
+    command.stdout.on("data", (line) => appendDockerProcessStream(`${line}`));
+    command.stderr.on("data", (line) => appendDockerProcessStream(`${line}`));
+    const child = await command.spawn();
+    setDockerProcessStream(`👉 Stopping Docker...`);
+  };
+
+  const startDocker = async () => {
+    setDockerModalOpen(true);
+    setDockerNeedsRestart(false);
+
+    await stopDocker();
+
+    await new Promise((resolve) => setTimeout(resolve, 2500));
+
+    const resourcePath = await resolveResource(
+      "bundle/templates/docker-compose.yml.template"
+    );
+    console.log(`resourcePath: ${resourcePath}`);
+    const dockerComposeTemplate = await readTextFile(resourcePath);
+
+    appendDockerProcessStream(`👉 Starting Docker...`);
+    await writeTextFile(`docker-compose.yml`, dockerComposeTemplate, {
+      dir: BaseDirectory.AppData,
+    });
+
+    const appDataDirPath = await appDataDir();
+
+    const command = new Command("run-docker-compose", [
+      "compose",
+      "-v",
+      "-f",
+      `${appDataDirPath}/docker-compose.yml`,
+      "up",
+      "-d",
+    ]);
+    command.on("close", (data) => {
+      if (data.code == 0) {
+        appendDockerProcessStream(`✅ Starting Docker successfully finished.`);
+      } else {
+        appendDockerProcessStream(
+          `🚨 Starting Docker failed with code ${data.code} and signal ${data.signal}`
+        );
+      }
+    });
+    command.on("error", (error) => console.error(`command error: "${error}"`));
+    command.stdout.on("data", (line) => appendDockerProcessStream(`${line}`));
+    command.stderr.on("data", (line) => appendDockerProcessStream(`${line}`));
+    const child = await command.spawn();
+    appendDockerProcessStream(`command spawned with pid ${child.pid}`);
+  };
+
+  return (
+    <>
+      <Button
+        variant={"default"}
+        onClick={() => {
+          if (endpointList.length === 0) {
+            return;
+          }
+          startDocker();
+        }}
+        className={cn(dockerNeedsRestart ? "animate-bounce" : "")}
+        disabled={endpointList.length === 0}
+      >
+        {dockerNeedsRestart ? "Restart Docker To Apply" : "Start Docker "}
+      </Button>
+      <DockerLogModal
+        stream={dockerProcessStream}
+        isOpen={dockerModalOpen}
+        onClosed={() => {
+          setDockerModalOpen(false);
+        }}
+      />
+    </>
+  );
+}
--- src/components/page-components/proxy-list/index.tsx
+++ src/components/page-components/proxy-list/index.tsx
@@ -1,18 +1,15 @@
 "use client";
 
-import { Button } from "@/components/ui/button";
 import { CertificateManager } from "@/helpers/certificate-manager";
 import { EndpointManager } from "@/helpers/endpoint-manager";
-import { cn } from "@/lib/utils";
 import { invoke } from "@tauri-apps/api";
 import { confirm } from "@tauri-apps/api/dialog";
-import { BaseDirectory, readTextFile, writeTextFile } from "@tauri-apps/api/fs";
-import { appDataDir, resolveResource } from "@tauri-apps/api/path";
-import { Command, open as shellOpen } from "@tauri-apps/api/shell";
+import { appDataDir } from "@tauri-apps/api/path";
+import { open as shellOpen } from "@tauri-apps/api/shell";
 import { useCallback, useEffect, useState } from "react";
+import DockerControl from "../docker-control";
 import { EndpointData } from "./add";
 import CreateProxyV2SideComponent from "./add-new";
-import DockerLogModal from "./docker-log";
 import RequestPasswordModal from "./request-certificate-trust";
 import EndpointListTable from "./table";
 
@@ -20,88 +17,9 @@ export default function EndpointListComponent() {
   const [loaded, setLoaded] = useState(false);
   const [endpointList, setEndpointList] = useState([]);
   const [openSide, setOpenSide] = useState(false);
-  const [dockerModalOpen, setDockerModalOpen] = useState(false);
-  const [dockerProcessStream, setDockerProcessStream] = useState<any>("");
+
   const [passwordModalShown, setPasswordModalOpen] = useState(false);
   const [currentEndpoint, setCurrentEndpoint] = useState<EndpointData>();
-  const [dockerNeedsRestart, setDockerNeedsRestart] = useState(false);
-
-  const appendDockerProcessStream = useCallback((line: any) => {
-    if (typeof line === "string") {
-      setDockerProcessStream((prev: any) => prev + `\n${line}`);
-    } else {
-      setDockerProcessStream((prev: any) => prev + line);
-    }
-  }, []);
-
-  const stopDocker = async () => {
-    const appDataDirPath = await appDataDir();
-    const command = new Command("stop-docker-compose", [
-      "compose",
-      "-f",
-      `${appDataDirPath}/docker-compose.yml`,
-      "down",
-    ]);
-    command.on("close", (data) => {
-      if (data.code == 0) {
-        appendDockerProcessStream(`✅ Stopping Docker successfully finished.`);
-      } else {
-        appendDockerProcessStream(
-          `🚨 Stopping Docker failed with code ${data.code} and signal ${data.signal}`
-        );
-      }
-      appendDockerProcessStream("💤 Waiting for docker to settle...");
-    });
-    command.on("error", (error) => console.error(`command error: "${error}"`));
-    command.stdout.on("data", (line) => appendDockerProcessStream(`${line}`));
-    command.stderr.on("data", (line) => appendDockerProcessStream(`${line}`));
-    const child = await command.spawn();
-    setDockerProcessStream(`👉 Stopping Docker...`);
-  };
-  const startDocker = async () => {
-    setDockerModalOpen(true);
-    setDockerNeedsRestart(false);
-
-    await stopDocker();
-
-    await new Promise((resolve) => setTimeout(resolve, 2500));
-
-    const resourcePath = await resolveResource(
-      "bundle/templates/docker-compose.yml.template"
-    );
-    console.log(`resourcePath: ${resourcePath}`);
-    const dockerComposeTemplate = await readTextFile(resourcePath);
-
-    appendDockerProcessStream(`👉 Starting Docker...`);
-    await writeTextFile(`docker-compose.yml`, dockerComposeTemplate, {
-      dir: BaseDirectory.AppData,
-    });
-
-    const appDataDirPath = await appDataDir();
-
-    const command = new Command("run-docker-compose", [
-      "compose",
-      "-v",
-      "-f",
-      `${appDataDirPath}/docker-compose.yml`,
-      "up",
-      "-d",
-    ]);
-    command.on("close", (data) => {
-      if (data.code == 0) {
-        appendDockerProcessStream(`✅ Starting Docker successfully finished.`);
-      } else {
-        appendDockerProcessStream(
-          `🚨 Starting Docker failed with code ${data.code} and signal ${data.signal}`
-        );
-      }
-    });
-    command.on("error", (error) => console.error(`command error: "${error}"`));
-    command.stdout.on("data", (line) => appendDockerProcessStream(`${line}`));
-    command.stderr.on("data", (line) => appendDockerProcessStream(`${line}`));
-    const child = await command.spawn();
-    appendDockerProcessStream(`command spawned with pid ${child.pid}`);
-  };
 
   const onDeleteFromHosts = useCallback(
     async (endpoint: EndpointData, password: string) => {
@@ -130,7 +48,6 @@ export default function EndpointListComponent() {
     invoke("remove_cert_from_keychain", {
       name: `${endpoint.hostname}`,
     });
-    setDockerNeedsRestart(true);
     setPasswordModalOpen(true);
   }, []);
 
@@ -150,7 +67,6 @@ export default function EndpointListComponent() {
     }
     endpointList.push(data);
     mgr.save(endpointList);
-    setDockerNeedsRestart(true);
     setEndpointList(endpointList);
   }, []);
 
@@ -164,13 +80,6 @@ export default function EndpointListComponent() {
   return (
     <div className="flex flex-col min-h-screen text-gray-100 bg-gray-900">
       <div className="">
-        <DockerLogModal
-          stream={dockerProcessStream}
-          isOpen={dockerModalOpen}
-          onClosed={() => {
-            setDockerModalOpen(false);
-          }}
-        />
         <CreateProxyV2SideComponent
           open={openSide}
           setOpen={setOpenSide}
@@ -206,19 +115,7 @@ export default function EndpointListComponent() {
           }}
         />
         <div className="flex gap-2 px-4 py-4 fixed top-0 left-0 right-0 bg-gray-700">
-          <Button
-            variant={"default"}
-            onClick={() => {
-              if (endpointList.length === 0) {
-                return;
-              }
-              startDocker();
-            }}
-            className={cn(dockerNeedsRestart ? "animate-bounce" : "")}
-            disabled={endpointList.length === 0}
-          >
-            {dockerNeedsRestart ? "Restart Docker To Apply" : "Start Docker "}
-          </Button>
+          <DockerControl endpointList={endpointList} />
           <div
             className="p-2 underline cursor-pointer text-sm"
             onClick={() => {
