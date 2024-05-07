0b3f8c57377217292c6af6b6399539b512c7b378
cheeselemon <cheeselemon.yarn@gmail.com>
2024-05-07 18:22:15 +0900

    feat: update tooltip and topnav menu

--- src/components/page-components/proxy-list/index.tsx
+++ src/components/page-components/proxy-list/index.tsx
@@ -1,5 +1,12 @@
 "use client";
 
+import DiscordIcon from "@/components/icons/discord";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 import { CertificateManager } from "@/helpers/certificate-manager";
 import { EndpointManager } from "@/helpers/endpoint-manager";
 import { invoke } from "@tauri-apps/api";
@@ -78,71 +85,102 @@ export default function EndpointListComponent() {
   const shouldShowAddButton = loaded;
 
   return (
-    <div className="flex flex-col min-h-screen text-gray-100 bg-gray-900">
-      <div className="">
-        <CreateProxyV2SideComponent
-          open={openSide}
-          setOpen={setOpenSide}
-          onAdd={(data) => {
-            setCurrentEndpoint(data);
-            addEndpoint(data);
-          }}
-        />
-        <RequestPasswordModal
-          description={
-            "Ophiuchi wants to edit: /etc/hosts. If you cancel, you can edit it manually."
-          }
-          isOpen={passwordModalShown}
-          onConfirm={function (password: string): void {
-            setPasswordModalOpen(false);
-            if (!currentEndpoint) return;
-            onDeleteFromHosts(currentEndpoint, password);
-            const endpointManager = EndpointManager.sharedManager();
-            const configHelper = new CertificateManager();
-            configHelper.deleteCertificateFiles(currentEndpoint.hostname);
-            configHelper.deleteNginxConfigurationFiles(
-              currentEndpoint.hostname
-            );
-
-            const copiedList = [...endpointList];
-            const index = copiedList.findIndex((e: EndpointData) => {
-              return e.hostname === currentEndpoint.hostname;
-            });
-            copiedList.splice(index, 1);
-
-            endpointManager.save(copiedList);
-            setEndpointList(copiedList);
-          }}
-        />
-        <div className="flex gap-2 px-4 py-4 fixed top-0 left-0 right-0 bg-gray-700">
-          <DockerControl endpointList={endpointList} />
-          <div
-            className="p-2 underline cursor-pointer text-sm"
-            onClick={() => {
-              openAppData();
+    <TooltipProvider delayDuration={0}>
+      <div className="flex flex-col min-h-screen text-gray-100 bg-gray-900">
+        <div className="">
+          <CreateProxyV2SideComponent
+            open={openSide}
+            setOpen={setOpenSide}
+            onAdd={(data) => {
+              setCurrentEndpoint(data);
+              addEndpoint(data);
             }}
-          >
-            Open docker-compose folder
+          />
+          <RequestPasswordModal
+            description={
+              "Ophiuchi wants to edit: /etc/hosts. If you cancel, you can edit it manually."
+            }
+            isOpen={passwordModalShown}
+            onConfirm={function (password: string): void {
+              setPasswordModalOpen(false);
+              if (!currentEndpoint) return;
+              onDeleteFromHosts(currentEndpoint, password);
+              const endpointManager = EndpointManager.sharedManager();
+              const configHelper = new CertificateManager();
+              configHelper.deleteCertificateFiles(currentEndpoint.hostname);
+              configHelper.deleteNginxConfigurationFiles(
+                currentEndpoint.hostname
+              );
+
+              const copiedList = [...endpointList];
+              const index = copiedList.findIndex((e: EndpointData) => {
+                return e.hostname === currentEndpoint.hostname;
+              });
+              copiedList.splice(index, 1);
+
+              endpointManager.save(copiedList);
+              setEndpointList(copiedList);
+            }}
+          />
+          <div className="flex justify-between w-full fixed top-0 left-0 right-0 bg-gray-700 px-6 py-4 ">
+            <div className="flex gap-2 items-center">
+              <DockerControl endpointList={endpointList} />
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <div
+                    className="p-2 underline cursor-pointer text-sm"
+                    onClick={() => {
+                      openAppData();
+                    }}
+                  >
+                    Open docker-compose folder
+                  </div>
+                </TooltipTrigger>
+                <TooltipContent side="bottom">
+                  <p>Opens the folder where docker-compose.yml is located.</p>
+                </TooltipContent>
+              </Tooltip>
+            </div>
+            <div className="flex gap-2 items-center">
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <a
+                    className="p-2 underline cursor-pointer text-sm"
+                    href={`https://heavenly-tent-fff.notion.site/Ophiuchi-Developers-Toolkit-734dc4f766fe40aebfe0da3cbbc304f5?pvs=4`}
+                    target="_blank"
+                  >
+                    Help
+                  </a>
+                </TooltipTrigger>
+                <TooltipContent side="bottom">
+                  <p>Open help docs.</p>
+                </TooltipContent>
+              </Tooltip>
+
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <a href="https://discord.gg/fpp8kNyPtz" target="_blank">
+                    <DiscordIcon className="w-4 h-4 text-white" />
+                  </a>
+                </TooltipTrigger>
+                <TooltipContent side="bottom" sideOffset={12}>
+                  <p>Get help from Discord.</p>
+                </TooltipContent>
+              </Tooltip>
+            </div>
           </div>
-          <a
-            className="p-2 underline cursor-pointer text-sm"
-            href={`https://heavenly-tent-fff.notion.site/Ophiuchi-Developers-Toolkit-734dc4f766fe40aebfe0da3cbbc304f5?pvs=4`}
-            target="_blank"
-          >
-            Help
-          </a>
+        </div>
+
+        <div className="p-4 mt-20">
+          <EndpointListTable
+            list={endpointList}
+            onDeleteEndpoint={onDeleteEndpoint}
+            onAddEndpoint={() => {
+              setOpenSide(true);
+            }}
+          />
         </div>
       </div>
-
-      <div className="p-4 mt-20">
-        <EndpointListTable
-          list={endpointList}
-          onDeleteEndpoint={onDeleteEndpoint}
-          onAddEndpoint={() => {
-            setOpenSide(true);
-          }}
-        />
-      </div>
-    </div>
+    </TooltipProvider>
   );
 }
--- src/components/page-components/proxy-list/table/index.tsx
+++ src/components/page-components/proxy-list/table/index.tsx
@@ -2,7 +2,6 @@ import { Button } from "@/components/ui/button";
 import {
   Tooltip,
   TooltipContent,
-  TooltipProvider,
   TooltipTrigger,
 } from "@/components/ui/tooltip";
 import { cn } from "@/lib/utils";
@@ -38,95 +37,94 @@ export default function EndpointListTable({
   }, []);
 
   return (
-    <TooltipProvider delayDuration={0}>
-      <div className="px-4">
-        <div className="sm:flex sm:items-center">
-          <div className="sm:flex-auto">
-            <h1 className="text-base font-semibold leading-6 text-white">
-              Your Proxies
-            </h1>
-            <p className="mt-2 text-sm text-gray-300">
-              List of proxies that are currently registered.
-            </p>
-          </div>
-          <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
-            <Button
-              variant={"default"}
-              className={cn(
-                "flex gap-2 items-center",
-                list.length === 0 ? "animate-bounce" : ""
-              )}
-              onClick={() => {
-                onAddEndpoint();
-              }}
-            >
-              <PlusIcon className="w-4 h-4" />
-              Create Proxy
-            </Button>
-          </div>
+    <div className="px-4">
+      <div className="sm:flex sm:items-center">
+        <div className="sm:flex-auto">
+          <h1 className="text-base font-semibold leading-6 text-white">
+            Your Proxies
+          </h1>
+          <p className="mt-2 text-sm text-gray-300">
+            List of proxies that are currently registered.
+          </p>
         </div>
-        <div className="mt-8 flow-root">
-          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
-            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
-              <table className="min-w-full divide-y divide-gray-700">
-                <thead>
-                  <tr>
-                    {/* <th
+        <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+          <Button
+            variant={"default"}
+            className={cn(
+              "flex gap-2 items-center",
+              list.length === 0 ? "animate-bounce" : ""
+            )}
+            onClick={() => {
+              onAddEndpoint();
+            }}
+          >
+            <PlusIcon className="w-4 h-4" />
+            Create Proxy
+          </Button>
+        </div>
+      </div>
+      <div className="mt-8 flow-root">
+        <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+          <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+            <table className="min-w-full divide-y divide-gray-700">
+              <thead>
+                <tr>
+                  {/* <th
                     scope="col"
                     className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-0"
                   >
                     Nickname
                   </th> */}
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-white sm:pl-0"
-                    >
-                      Hostname
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-white"
-                    >
-                      Application Port
-                    </th>
-                    {/* <th
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-white sm:pl-0"
+                  >
+                    Hostname
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                  >
+                    Application Port
+                  </th>
+                  {/* <th
                     scope="col"
                     className="px-3 py-3.5 text-left text-sm font-semibold text-white"
                   >
                     Actions
                   </th> */}
-                    {/* <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
+                  {/* <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-0">
                     <span className="sr-only">Delete</span>
                   </th> */}
-                  </tr>
-                </thead>
+                </tr>
+              </thead>
 
-                <tbody className="divide-y divide-gray-800">
-                  {list.map((endpoint) => (
-                    <tr key={endpoint.nickname}>
-                      {/* <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
+              <tbody className="divide-y divide-gray-800">
+                {list.map((endpoint) => (
+                  <tr key={endpoint.nickname}>
+                    {/* <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
                       {endpoint.nickname}
                     </td> */}
-                      <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
-                        <Tooltip>
-                          <TooltipTrigger asChild>
-                            <a
-                              className="p-2 underline cursor-pointer text-sm sm:pl-0"
-                              href={`https://${endpoint.hostname}`}
-                              target="_blank"
-                            >
-                              {endpoint.hostname}
-                            </a>
-                          </TooltipTrigger>
-                          <TooltipContent side="right">
-                            <p>Click to open on browser.</p>
-                          </TooltipContent>
-                        </Tooltip>
-                      </td>
-                      <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
-                        {endpoint.port}
-                      </td>
-                      {/* <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-0">
+                      <Tooltip>
+                        <TooltipTrigger asChild>
+                          <a
+                            className="p-2 underline cursor-pointer text-sm sm:pl-0"
+                            href={`https://${endpoint.hostname}`}
+                            target="_blank"
+                          >
+                            {endpoint.hostname}
+                          </a>
+                        </TooltipTrigger>
+                        <TooltipContent side="right">
+                          <p>Click to open on browser.</p>
+                        </TooltipContent>
+                      </Tooltip>
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
+                      {endpoint.port}
+                    </td>
+                    {/* <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-300">
                       <p
                         className="underline cursor-pointer"
                         onClick={() => {
@@ -136,36 +134,35 @@ export default function EndpointListTable({
                         Trust SSL Cert
                       </p>
                     </td> */}
-                      <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
-                        <div className="flex gap-6 justify-end">
-                          <p
-                            onClick={() => {
-                              openCert(endpoint);
-                            }}
-                            className="text-indigo-400 hover:text-indigo-300 cursor-pointer"
-                          >
-                            Locate Cert
-                            <span className="sr-only">, {endpoint.name}</span>
-                          </p>
-                          <p
-                            onClick={() => {
-                              onDeleteEndpoint(endpoint);
-                            }}
-                            className="text-red-400 hover:text-red-300 cursor-pointer"
-                          >
-                            Delete
-                            <span className="sr-only">, {endpoint.name}</span>
-                          </p>
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
+                    <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-0">
+                      <div className="flex gap-6 justify-end">
+                        <p
+                          onClick={() => {
+                            openCert(endpoint);
+                          }}
+                          className="text-indigo-400 hover:text-indigo-300 cursor-pointer"
+                        >
+                          Locate Cert
+                          <span className="sr-only">, {endpoint.name}</span>
+                        </p>
+                        <p
+                          onClick={() => {
+                            onDeleteEndpoint(endpoint);
+                          }}
+                          className="text-red-400 hover:text-red-300 cursor-pointer"
+                        >
+                          Delete
+                          <span className="sr-only">, {endpoint.name}</span>
+                        </p>
+                      </div>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
           </div>
         </div>
       </div>
-    </TooltipProvider>
+    </div>
   );
 }
